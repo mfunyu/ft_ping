c882ae2302a26cabce9c967cf1c4e6cabcc291d4
mfunyu <mfunyu@student.42lyon.fr>
2023-12-20 15:56:42 +0100

    working

--- srcs/ft_ping.c
+++ srcs/ft_ping.c
@@ -8,10 +8,29 @@ void	error_exit(char *msg)
 	exit(EXIT_FAILURE);
 }
 
+void	parse_args(int ac, char **av)
+{
+	char	*arg;
+
+	for (int i = 1; i < ac; i++)
+	{
+		arg = av[i];
+		printf("%s\n", arg);
+		if (arg[0] == '-')
+		{
+
+		}
+		else
+		{
+			
+		}
+	}
+}
+
 int	main(int ac, char **av)
 {
 	if (ac <= 1)
 		error_exit("usage error: Destination address required");
-	(void)av;
+	parse_args(ac, av);
 	return (0);
 }
