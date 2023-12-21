92adfa3c36bb63f6e022e42c7ea600b0b8bdfb6e
mfunyu <mfunyu@student.42lyon.fr>
2023-12-21 17:45:43 +0100

    refac: use t_flags

--- srcs/parser.c
+++ srcs/parser.c
@@ -52,55 +52,55 @@ t_flags	match_short_option(char option)
 	return (NONE);
 }
 
-int	parse_option(char *option, t_args *args)
+t_flags	parse_option(char *option, t_args *args)
 {
+	t_flags	match;
 	t_flags	flag;
-	int		value_flag;
 
 	if (option[1] == '-')
 	{
-		flag = match_long_option(option);
-		args->flags[flag] = 1;
-		return (0);
+		match = match_long_option(option);
+		args->flags[match] = 1;
+		return (NONE);
 	}
-	value_flag = 0;
+	flag = NONE;
 	for (int j = 1; j < (int)ft_strlen(option); j++)
 	{
-		if (value_flag)
+		if (flag)
 		{
-			args->flags[value_flag] = parse_value(option + j);
-			return (0);
+			args->flags[flag] = parse_value(option + j);
+			return (NONE);
 		}
-		flag = match_short_option(option[j]);
-		args->flags[flag] = 1;
-		if (options[flag - 1].req_value)
-			value_flag = flag;
+		match = match_short_option(option[j]);
+		args->flags[match] = 1;
+		if (options[match - 1].req_value)
+			flag = match;
 	}
-	return (value_flag);
+	return (flag);
 }
 
 t_args	parse_args(int ac, char **av)
 {
 	t_args		args = {0};
-	int			value_flag;
+	t_flags		flag;
 	int			idx;
 
 	args.params = av;
-	value_flag = 0;
+	flag = NONE;
 	idx = 0;
 	for (int i = 1; i < ac; i++)
 	{
-		if (value_flag)
+		if (flag)
 		{
-			args.flags[value_flag] = parse_value(av[i]);
-			value_flag = 0;
+			args.flags[flag] = parse_value(av[i]);
+			flag = NONE;
 		}
 		else if (av[i][0] == '-' && av[i][1])
-			value_flag = parse_option(av[i], &args);
+			flag = parse_option(av[i], &args);
 		else
 			args.params[idx++] = av[i];
 	}
-	if (value_flag)
+	if (flag)
 		error_exit_usage("option requires an argument -- 'c'");
 	args.params[idx] = NULL;
 	return (args);
