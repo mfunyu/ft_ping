b14907ca50d396eb6c1e807a2737162bd0cc1237
mfunyu <mfunyu@student.42lyon.fr>
2023-12-21 17:16:51 +0100

    update: function separation wip

--- srcs/parser.c
+++ srcs/parser.c
@@ -14,17 +14,18 @@ static t_options	options[] = {
 	{'\0', "\0",		NONE,	false}
 };
 
-void	parse_value(t_args *args, int value_flag, char *value)
+int	parse_value(char *value)
 {
 	int	error;
+	int	ret;
 
-	printf("%s\n", value);
-	args->flags[value_flag] = ft_atoi_check(value, &error);
+	ret = ft_atoi_check(value, &error);
 	if (error == ERROR)
 		error_exit("invalid value");
+	return (ret);
 }
 
-void	match_long_option(char *option, t_args *args)
+t_flags	match_long_option(char *option)
 {
 	int	diff;
 
@@ -32,42 +33,21 @@ void	match_long_option(char *option, t_args *args)
 	{
 		diff = ft_strncmp(option, options[i].long_option, ft_strlen(options[i].long_option));
 		if (!diff)
-		{
-			args->flags[options[i].flag] = 1;
-			return ;
-		}
+			return (options[i].flag);
 	}
 	error_exit_usage("unrecognized option ''");
+	return (NONE);
 }
 
-t_flags	match_short_option(char *option, t_args *args)
+t_flags	match_short_option(char option)
 {
-	t_flags		value;
-	t_options	current;
-
-	value = NONE;
-	for (int i = 1; i < (int)ft_strlen(option); i++)
+	for (int i = 0; options[i].flag; i++)
 	{
-		if (value)
-		{
-			parse_value(args, value, option + i);
-			return (NONE);
-		}
-		for (int j = 0;; j++)
-		{
-			current = options[j];
-			if (!current.flag)
-				error_exit_usage("invalid option -- ");
-			if (option[i] == current.short_option)
-			{
-				args->flags[current.flag] = 1;
-				if (current.req_value)
-					value = current.flag;
-				break ;
-			}
-		}
+		if (option == options[i].short_option)
+			return (options[i].flag);
 	}
-	return (value);
+	error_exit_usage("invalid option -- ");
+	return (NONE);
 }
 
 t_args	parse_args(int ac, char **av)
@@ -75,6 +55,7 @@ t_args	parse_args(int ac, char **av)
 	t_args		args = {0};
 	int			value_flag;
 	int			idx;
+	t_flags		flag;
 
 	args.params = av;
 	value_flag = 0;
@@ -83,15 +64,31 @@ t_args	parse_args(int ac, char **av)
 	{
 		if (value_flag)
 		{
-			parse_value(&args, value_flag, av[i]);
+			args.flags[value_flag] = parse_value(av[i]);
 			value_flag = 0;
 		}
 		else if (av[i][0] == '-' && av[i][1])
 		{
 			if (av[i][1] == '-')
-				match_long_option(av[i], &args);
+			{
+				flag = match_long_option(av[i]);
+				args.flags[flag] = 1;
+			}
 			else
-				value_flag = match_short_option(av[i], &args);
+			{
+				for (int j = 1; j < (int)ft_strlen(av[i]); j++)
+				{
+					if (value_flag)
+					{
+						args.flags[value_flag] = parse_value(av[i] + j);
+						break ;
+					}
+					flag = match_short_option(av[i][j]);
+					args.flags[flag] = 1;
+					if (options[flag].req_value)
+						value_flag = flag;
+				}
+			}
 		}
 		else
 			args.params[idx++] = av[i];
