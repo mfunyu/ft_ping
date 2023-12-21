62332622720d7ebff7ac7c6ab59972109238b5f9
mfunyu <mfunyu@student.42lyon.fr>
2023-12-21 17:27:02 +0100

    add: help, and no param error

--- srcs/ft_ping.c
+++ srcs/ft_ping.c
@@ -1,5 +1,15 @@
 #include "ft_ping.h"
 #include "error.h"
+#include <stdio.h>
+
+void	help(void)
+{
+	printf("Usage: ping [OPTION...] HOST ...\n\
+Send ICMP ECHO_REQUEST packets to network hosts.\n\n\
+ Options valid for all request types:\n\
+  -v, --verbose              verbose output\n\n\
+  -?, --help                 give this help list\n\n");
+}
 
 int	main(int ac, char **av)
 {
@@ -8,6 +18,13 @@ int	main(int ac, char **av)
 	if (ac <= 1)
 		error_exit_usage("missing host operand");
 	args = parse_args(ac, av);
+	if (args.flags[HELP])
+	{
+		help();
+		return (0);
+	}
+	if (!args.params[0])
+		error_exit_usage("missing host operand");
 	print_args(args);
 	return (0);
 }
