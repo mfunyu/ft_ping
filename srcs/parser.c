20480ecae84680db9e9543092506bc030ef91e67
mfunyu <mfunyu@student.42lyon.fr>
2024-01-09 16:00:04 +0100

    fix: handle args in each parse option func

--- srcs/parser.c
+++ srcs/parser.c
@@ -28,36 +28,6 @@ int	parse_value(char *value)
 	return (ret);
 }
 
-int	parse_value2(char *option, char **av, bool is_short) // no value, pass current option and av
-{
-	(void)option;
-	(void)av;
-	(void)is_short;
-	/*
-	// check case of '-c123'
-	if (is_short)
-	{
-		if (option[1])
-			return (parse_value(option[1]));
-	}
-	// check case of '--c=123'
-	else
-	{
-		argument = ft_strchr(option, '=');
-		if (argument)
-			return (get_value(argument));
-	}
-	// check next of av
-	if (av + 1)
-		return (get_value(av + 1));
-
-	if (is_short)
-		error_exit_value("option requires an argument --", option);
-	else
-		error_exit_value("option '' requires an argument");*/
-		return (0);
-}
-
 int	match_long_option(char *option)
 {
 	int		c;
@@ -74,7 +44,7 @@ int	match_long_option(char *option)
 	return (0);
 }
 
-int	parse_long_option(char **av, t_args *args)
+bool	parse_long_option(char **av, t_args *args)
 {
 	char	*option;
 	int		idx;
@@ -88,12 +58,17 @@ int	parse_long_option(char **av, t_args *args)
 		if (!g_options[idx].req_value)
 			error_exit_usage("ping: option '--xxx' doesn't allow an argument");
 		args->flags[flag] = parse_value(option + ft_strlen("--"));
-		return (-1);
+		return (false);
 	}
 	args->flags[flag] = 1;
 	if (g_options[idx].req_value)
-		return (idx);
-	return (-1);
+	{
+		if (!av[1])
+			error_exit_value("option requires an argument --", g_options[idx].long_option);
+		args->flags[flag] = parse_value(av[1]);
+		return (true);
+	}
+	return (false);
 }
 
 int		match_short_option(char *option)
@@ -107,7 +82,7 @@ int		match_short_option(char *option)
 	return (0);
 }
 
-int	parse_short_option(char **av, t_args *args)
+bool	parse_short_option(char **av, t_args *args)
 {
 	char	*option;
 	int		idx;
@@ -117,51 +92,48 @@ int	parse_short_option(char **av, t_args *args)
 	for (int j = 1; j < (int)ft_strlen(option); j++)
 	{
 		idx = match_short_option(option + j);
-		printf("idx: %d\n", idx);
 		flag = g_options[idx].flag;
 		args->flags[flag] = 1;
 		if (g_options[idx].req_value)
 		{
-			if (!option[j + 1])
-				return (idx);
-			args->flags[flag] = parse_value(option + j + 1);
-			break ;
+			if (option[j + 1])
+			{
+				args->flags[flag] = parse_value(option + j + 1);
+				return (false);
+			}
+			if (!av[1])
+				error_exit_value("option requires an argument --", &g_options[idx].short_option);
+			args->flags[flag] = parse_value(av[1]);
+			return (true);
 		}
 	}
-	return (-1);
+	return (false);
 }
 
 t_args	parse_args(int ac, char **av)
 {
-	t_args		args = {0};
-	int			idx;
-	int			ret;
+	t_args	args = {0};
+	int		idx;
+	bool	next;
 
 	args.params = av;
 	idx = 0;
-	ret = -1;
+	next = false;
 	for (int i = 1; i < ac; i++)
 	{
 		if (args.flags[HELP])
 			return (args);
-		if (ret >= 0)
-		{
-			args.flags[ret] = parse_value(av[i]);
-			ret = -1;
-		}
-		else if (av[i][0] == '-' && av[i][1])
+		if (av[i][0] == '-' && av[i][1])
 		{
 			if (av[i][1] == '-')
-				ret = parse_long_option(av + i, &args);
+				next = parse_long_option(av + i, &args);
 			else
-				ret = parse_short_option(av + i, &args);
-			printf("ret: %d\n", ret);
+				next = parse_short_option(av + i, &args);
+			i += next;
 		}
 		else
 			args.params[idx++] = av[i];
 	}
-	if (ret >= 0)
-		error_exit_value("option requires an argument --", g_options[ret].long_option);
 	args.params[idx] = NULL;
 	return (args);
 }
