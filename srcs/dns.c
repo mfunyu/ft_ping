a204cfee01f2cf601c45140c641f7a41ed658853
mfunyu <mfunyu@student.42tokyo.jp>
2024-01-19 13:49:04 +0100

    anyway2

--- srcs/dns.c
+++ srcs/dns.c
@@ -4,17 +4,18 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 
 int dns_resolution(char const *hostname)
 {
 	int s;
+	int sfd;
 	struct addrinfo *result;
-	struct addrinfo *rp;
 	struct addrinfo hints;
 
 	memset(&hints, 0, sizeof(hints));
 	hints.ai_family = AF_INET;
-	hints.ai_socktype = SOCK_DGRAM;
+	hints.ai_socktype = SOCK_RAW;
 	hints.ai_protocol = IPPROTO_ICMP;
 
 	s = getaddrinfo(hostname, NULL, &hints, &result);
@@ -23,20 +24,21 @@ int dns_resolution(char const *hostname)
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
 		exit(EXIT_FAILURE);
 	}
-	for (rp = result; rp != NULL; rp = rp->ai_next)
-	{
-		printf("Family: %d\n", rp->ai_family);
-		printf("Socktype: %d\n", rp->ai_socktype);
-		printf("Protocol: %d\n", rp->ai_protocol);
-		printf("Addr len: %d\n", rp->ai_addrlen);
-		//printf("Addr: %s\n", rp->ai_addr);
-	}
-	freeaddrinfo(result);
-
-	if (rp == NULL)
+	if (result == NULL)
 	{
 		fprintf(stderr, "Could not connect\n");
 		exit(EXIT_FAILURE);
 	}
+		printf("Family: %d\n", result->ai_family);
+		printf("Socktype: %d\n", result->ai_socktype);
+		printf("Protocol: %d\n", result->ai_protocol);
+		printf("Addr len: %d\n", result->ai_addrlen);
+		sfd = socket(PF_INET, SOCK_DGRAM, IPPROTO_ICMP);
+		if (sfd == -1)
+			exit(EXIT_FAILURE);
+		if (connect(sfd, result->ai_addr, result->ai_addrlen) == -1)
+			exit(EXIT_SUCCESS);
+	freeaddrinfo(result);
+	printf("Connected\n");
 	return (0);
 }
