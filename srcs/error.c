fe3df858fb6bb138f83d4424a1cf5ff820f679e4
mfunyu <mfunyu@student.42lyon.fr>
2024-02-06 13:26:10 +0100

    update: print strerror

--- srcs/error.c
+++ srcs/error.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <string.h>
 
 void	error_exit_usage(char *msg)
 {
@@ -11,6 +13,9 @@ void	error_exit_usage(char *msg)
 
 void	error_exit(char *msg)
 {
-	fprintf(stderr, "ft_ping: %s\n", msg);
+	fprintf(stderr, "ft_ping: %s", msg);
+	if (errno)
+		fprintf(stderr, " - %s", strerror(errno));
+	fprintf(stderr, "\n");
 	exit(EXIT_FAILURE);
 }
