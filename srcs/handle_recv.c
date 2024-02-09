960520a9219dc206c20106de901b473912333e22
mfunyu <mfunyu@student.42lyon.fr>
2024-02-09 10:46:21 +0100

    fix: print format for timetrip

--- srcs/handle_recv.c
+++ srcs/handle_recv.c
@@ -44,14 +44,7 @@ static void	_print_timetrip(struct timeval *tv_start, struct timeval *tv_end)
 	triptime = (tv_end->tv_sec - tv_start->tv_sec) * 1000 * 1000;
 	triptime += tv_end->tv_usec - tv_start->tv_usec;
 
-	if (triptime >= 100000)
-		printf(" time=%ld ms", triptime / 1000);
-	else if (triptime >= 10000)
-		printf(" time=%ld.%01ld ms", triptime / 1000, triptime % 1000 / 100);
-	else if (triptime >= 1000)
-		printf(" time=%ld.%02ld ms", triptime / 1000, triptime % 1000 / 10);
-	else
-		printf(" time=%ld.%03ld ms", triptime / 1000, triptime % 1000);
+	printf(" time=%ld,%03ld ms", triptime / 1000, triptime % 1000);
 }
 
 static void	_print_stats(t_icmp_send *send, struct timeval *tv)
