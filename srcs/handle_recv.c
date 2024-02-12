7f89787c9514bee657ae69ce060731b2278aea93
mfunyu <mfunyu@student.42lyon.fr>
2024-02-12 15:26:06 +0100

    update: set sockaddr_in for msg_name

--- srcs/handle_recv.c
+++ srcs/handle_recv.c
@@ -6,33 +6,37 @@
 #include <errno.h>
 #include <stdio.h>
 #include "print.h"
+#include <netinet/in.h>
+#include <arpa/inet.h>
 
 void	analyse_packet(ssize_t ret, struct msghdr *msg, t_icmp_send *send)
 {
 	char *buf = msg->msg_iov->iov_base;
 
 	printf("addrbuf: %s\n", buf);
+	(void)ret;
 	struct icmphdr *hdr;
-	hdr = (struct icmphdr *)(buf + 20);
+	hdr = (struct icmphdr *)(buf + sizeof(struct iphdr));
 	printf("type: %02x\n", hdr->type);
+	printf("msg %s\n", inet_ntoa(((struct sockaddr_in*)msg->msg_name)->sin_addr));
 
 	print_recv(hdr->type, msg, send);
 }
 
 int	receive_packet(int sfd, struct msghdr *msg)
 {
-	char		buf[1024];
-	char		addrbuf[1024];
-	struct iovec	iov;
-	ssize_t			ret;
+	char				buf[1024];
+	struct sockaddr_in	src_addr;
+	struct iovec		iov;
+	ssize_t				ret;
 
 	memset(msg, 0, sizeof(*msg));
 	iov.iov_base = buf;
 	iov.iov_len = 1024;
 	msg->msg_iov = &iov;
 	msg->msg_iovlen = 1;
-	msg->msg_name = addrbuf;
-	msg->msg_namelen = sizeof(addrbuf);
+	msg->msg_name = &src_addr;
+	msg->msg_namelen = sizeof(src_addr);
 
 	ret = recvmsg(sfd, msg, 0);
 	return (ret);
