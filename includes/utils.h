772ccc5563defba275e227e648fbd284e9e03456
mfunyu <mfunyu@student.42lyon.fr>
2024-02-27 14:01:02 +0100

    add: timeout setting for select

--- includes/utils.h
+++ includes/utils.h
@@ -2,7 +2,8 @@
 # define UTLIS_H
 
 void	get_current_timestamp(struct timeval *tv);
-double	calc_time_diff(struct timeval *tv1, struct timeval *tv2);
+struct timeval	calc_time_sub(struct timeval tv1, struct timeval tv2);
+double	calc_time_diff(struct timeval tv1, struct timeval tv2);
 double	calc_sqrt(double x, double precision);
 
 #endif /* UTILS_H */
--- srcs/utils.c
+++ srcs/utils.c
@@ -8,12 +8,41 @@ void	get_current_timestamp(struct timeval *tv)
 		error_exit("gettimeofday error");
 }
 
-double	calc_time_diff(struct timeval *tv1, struct timeval *tv2)
+void	calc_time_normalize(struct timeval *tv)
+{
+	while (tv->tv_usec < 0)
+	{
+		tv->tv_sec--;
+		tv->tv_usec += 1000000;
+	}
+	while (tv->tv_usec >= 1000000)
+	{
+		tv->tv_sec++;
+		tv->tv_usec -= 1000000;
+	}
+	if (tv->tv_sec < 0)
+	{
+		tv->tv_sec = 0;
+		tv->tv_usec = 0;
+	}
+}
+
+struct timeval	calc_time_sub(struct timeval tv1, struct timeval tv2)
+{
+	struct timeval	sub = {
+		.tv_sec = tv2.tv_sec - tv1.tv_sec,
+		.tv_usec = tv2.tv_usec - tv1.tv_usec,
+	};
+	calc_time_normalize(&sub);
+	return (sub);
+}
+
+double	calc_time_diff(struct timeval tv1, struct timeval tv2)
 {
 	double	diff;
 
-	diff = (tv2->tv_sec - tv1->tv_sec) * 1000;
-	diff += ((double)tv2->tv_usec - (double)tv1->tv_usec) / 1000;
+	diff = (tv2.tv_sec - tv1.tv_sec) * 1000;
+	diff += ((double)tv2.tv_usec - (double)tv1.tv_usec) / 1000;
 	return (diff);
 }
 
