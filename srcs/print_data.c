716f1c57c6c6e5597444f2db5c471f479d6e3db1
mfunyu <mfunyu@student.42lyon.fr>
2024-03-01 13:46:27 +0100

    update: separate _print_stat func

--- srcs/print_data.c
+++ srcs/print_data.c
@@ -28,19 +28,23 @@ void	print_header(t_ping ping)
 	printf("PING %s (%s): %ld data bytes\n", ping.dst_hostname, ping.dst_ip, ping.len - sizeof(struct icmphdr));
 }
 
-void	print_footer(t_ping ping)
+static void	_print_stat(t_stat stats)
 {
 	double	avg;
 	double	variance;
 
+	avg = stats.sum / stats.recieved;
+	variance = stats.sum_sq / stats.recieved - avg * avg;
+	printf("round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/%.3f ms\n",
+		stats.min, avg, stats.max, calc_sqrt(variance, 0.0005));
+}
+
+void	print_footer(t_ping ping)
+{
 	printf("--- %s ping statistics ---\n", ping.dst_hostname);
 	printf("%zu packets transmitted, %zu packets received, %zu%% packet loss\n",
 		ping.num_xmit, ping.stats.recieved, (ping.num_xmit - ping.stats.recieved) * 100 / ping.num_xmit);
-	if (ping.stats.recieved == 0)
-		return ;
 
-	avg = ping.stats.sum / ping.stats.recieved;
-	variance = ping.stats.sum_sq / ping.stats.recieved - avg * avg;
-	printf("round-trip min/avg/max/stddev = %.3f/%.3f/%.3f/%.3f ms\n",
-		ping.stats.min, avg, ping.stats.max, calc_sqrt(variance, 0.0005));
+	if (ping.stats.recieved)
+		_print_stat(ping.stats);
 }
