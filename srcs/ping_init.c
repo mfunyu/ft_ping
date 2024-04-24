3b6016af7bbbb278975251d05aa74d7e9e70f392
mfunyu <mfunyu@student.42lyon.fr>
2024-04-24 15:24:35 +0200

    hotfix: resolve #29, create socket first

--- srcs/ping_init.c
+++ srcs/ping_init.c
@@ -42,10 +42,10 @@ void	ping_init(t_ping *ping, t_args *args)
 	if (args->flags[VERBOSE])
 		ping->verbose = true;
 
+	ping->sfd = _create_socket(ping->ttl);
 	set_sockaddr_by_hostname(&ping->dst_addr, ping->dst_hostname);
 	set_ip_by_sockaddr(ping->dst_ip, &ping->dst_addr);
 	ping->ident = getpid();
 	ping->interval.tv_sec = 0;
 	ping->interval.tv_usec = PING_DEFAULT_INTERVAL_S * 1000000;
-	ping->sfd = _create_socket(ping->ttl);
 }
